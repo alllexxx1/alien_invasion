4b89376ac1b5b526777c7aec41b57a66a66c5f1d
alllexxx1 <alekkorpachyov@gmail.com>
2023-08-17 13:20:42 +0300

    Add movement to the game
    Udate method '_check_events' to add movement a ship as a reaction to keybord hint'
    Update a class 'Ship'
    Update a class 'Settings' by adding opportunity to adjust ship speed

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,3 +4,4 @@ class Settings:
         self.screen_width = 1700
         self.screen_height = 950
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.7
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,16 +1,29 @@
 import pygame
+from alien_invasion.settings import Settings
 
 
 class Ship:
     """A spaceship control class"""
     def __init__(self, game):
         self.screen = game.screen
+        self.settings = game.settings
         self.screen_rect = game.screen.get_rect()
 
         self.image = pygame.image.load('/home/aleksei/projects/alien_invasion'
                                        '/alien_invasion/images/ship.png')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
