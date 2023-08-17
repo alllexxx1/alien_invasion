408ed0370301c59b241f1ea8feabcee51a10b87e
alllexxx1 <alekkorpachyov@gmail.com>
2023-08-17 14:51:27 +0300

    Create module 'bullet.py' to implement logic of ship's bullet.\nAdd settings of bullet to 'settings.py'

---
+++ alien_invasion/bullet.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    def __init__(self, game):
+        """"A bullet control class"""
+        super().__init__()
+        self.screen = game.screen
+        self.settings = game.settings
+        self.color = self.settings.bullet_color
+
+        self.rect = pygame.Rect(0, 0, self.settings.screen_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = game.ship.rect.midtop
+
+        self.y = float(self.rect.y)
+
+    def update(self):
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,5 +3,9 @@ class Settings:
     def __init__(self):
         self.screen_width = 1700
         self.screen_height = 950
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (85, 206, 250)
         self.ship_speed = 1.7
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (216, 31, 42)
