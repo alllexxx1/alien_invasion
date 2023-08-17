e3fb2437c9f1ae67bdb7cf9541373d55c5d9b453
alllexxx1 <alekkorpachyov@gmail.com>
2023-08-17 10:05:02 +0300

    Create an entry point for game

---
+++ alien_invasion/scripts/start_game.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+
+from alien_invasion.alien_invasion import AlienInvasion
+
+
+def main():
+    game = AlienInvasion()
+    game.run_game()
+
+
+if __name__ == '__main__':
+    main()
