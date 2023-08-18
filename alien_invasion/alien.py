8a06c130ef01e9fe3c2f8ab72c5840e4802182f7
alllexxx1 <alekkorpachyov@gmail.com>
2023-08-18 18:54:46 +0300

    Create multiple rows of aliens and make them move from one edge of the screen to the other

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
     def __init__(self, game):
         super().__init__()
         self.screen = game.screen
+        self.settings = game.settings
 
         self.image = pygame.image.load('/home/aleksei/projects/alien_invasion'
                                        '/alien_invasion/images/alien.png')
@@ -16,3 +17,13 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         self.x = float(self.rect.x)
+
+    def update(self):
+        self.x += (self.settings.alien_speed *
+                   self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
