c0a77b8af91b0b813d924db49171f3654ec42047
alllexxx1 <alekkorpachyov@gmail.com>
2023-08-27 17:33:31 +0300

    Add displaying the current level

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -59,6 +59,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             self.aliens.empty()
             self.bullets.empty()
@@ -113,6 +114,9 @@ class AlienInvasion:
             self.bullets.empty()
             self.settings.increase_speed()
             self._create_fleet()
+
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         self._check_fleet_edges()
